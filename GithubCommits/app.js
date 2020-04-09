5008c5e908c28f695ce152b4eca9d2aced27f886
alexleybourne <alexleybourne1@gmail.com>
2020-04-09 17:54:58 +1000

    can randomly grab days now

--- GithubCommits/app.js
+++ GithubCommits/app.js
@@ -4,14 +4,21 @@ const Start = () => {
     CreateSquares()
 }
 
+const RandomColour = () => {
+    x = (Math.round(Math.random() * 52))
+    y = (Math.round(Math.random() * 7))
+    let day = document.querySelector(`#week${x} #day${y}`)
+    day.removeAttribute('class', `blank`)
+    day.setAttribute('class', `green square`)
+}
 
 const CreateSquares = () => {
     let x = 1
     let y = 1
 
-    innerCode = `<div class="square"></div>`
     divCode = ''
     while ( y < 8 ) {
+        innerCode = `<div id="day${y}" class="square blank"></div>`
         divCode = divCode + innerCode
         y ++
     }
@@ -20,7 +27,7 @@ const CreateSquares = () => {
         let area = document.getElementById('squareArea')
         let div = document.createElement('div')
         div.innerHTML = divCode
-        div.setAttribute('class', `week${x}`)
+        div.setAttribute('id', `week${x}`)
         area.appendChild(div)
         x ++ 
     }
